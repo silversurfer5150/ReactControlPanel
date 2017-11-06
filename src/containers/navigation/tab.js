b52b561854284d84816ceaecb2e0f41f282b5a08
Will Edwards <willedwards@lycos.com>
2017-11-06 20:54:31 +0100

    Update current selector

--- src/containers/navigation/tab.js
+++ src/containers/navigation/tab.js
@@ -7,21 +7,30 @@ import { collapseLeadspace, expandLeadspace, switchPanel } from '../../actions';
 const selectPanel = (props, e) => {
   const panelType = e.target.getAttribute('id');
   // collapse menu for more viewport if not home page - assigns the style class for header here also
-  panelType === 'db-tab-Welcome' ? props.expandLeadspace(panelType) : props.collapseLeadspace(panelType);
+  panelType === '' ? props.expandLeadspace() : props.collapseLeadspace(panelType);
 };
 
-const Tab = props => (
-  <NavLink to={props.path}>
-    <button
-      type="button"
-      id={`db-tab-${props.name}`}
-      className={`btn btn-lg ${props.class} ${props.current === props.path ? 'db-active-panel' : ''}`}
-      onClick={e => selectPanel(props, e)}
-    >
-      {props.name}
-    </button>
-  </NavLink>
-);
+class Tab extends React.Component {
+  componentWillMount() {
+    const panelType = this.props.current !== '/' ? `${this.props.current.replace(/^\/+/g, '')}` : 'default';
+    this.props.current === '/' ? this.props.expandLeadspace() : this.props.collapseLeadspace(panelType);
+  }
+  render() {
+    const props = this.props;
+    return (
+      <NavLink to={props.path}>
+        <button
+          type="button"
+          id={props.path.replace(/^\/+/g, '')}
+          className={`btn btn-lg ${props.class} ${props.current === props.path ? 'db-active-panel' : ''}`}
+          onClick={e => selectPanel(props, e)}
+        >
+          {props.name}
+        </button>
+      </NavLink>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   tabs: state.panel.tabs,
